dadbc70ad265f21917aa53ea6dcd6f86046aa561
pcxweb <1214840825@qq.com>
2018-03-27 18:35:54 +0800

    Add files via upload

---
+++ weixinchar/js/H5Componentcontent.js
@@ -0,0 +1,45 @@
+
+//问题1
+var listcon1 = `<h4>1.先把地理位置弄清楚，人家住在哪里知道吗？（单选）</h4>
+<ul><li><label for="radio1"><input id="radio1" type="radio" name="radio" value="1">波士顿 Boston</label></li>
+<li><label for="radio2"><input id="radio2" type="radio" name="radio" value="2">剑桥 Cambridge</label></li>
+<li><label for="radio3"><input id="radio3" type="radio" name="radio" value="3">伍斯特 Worcester</label></li>
+<li><label for="radio4"><input id="radio4" type="radio" name="radio" value="4">韦尔斯利 Wellesley</label></li></ul>`
+
+//问题2
+var listcon2 = `<h4>2.克拉克有多么声名远扬呢？（多选）</h4>
+<ul><li><label for="checkbox1"><input id="checkbox1" type="checkbox" name="checkbox1" value="1">全美第二所以研究院建校大学</label></li>
+<li><label for="checkbox2"><input id="checkbox2" type="checkbox" name="checkbox2" value="2">全美顶尖心理学院之一</label></li>
+<li><label for="checkbox3"><input id="checkbox3" type="checkbox" name="checkbox3" value="3">中国近代诗人徐志摩的母校</label></li>
+<li><label for="checkbox4"><input id="checkbox4" type="checkbox" name="checkbox4" value="4">众多知名校友，包括宗教领袖、美国“太空之父”、高盛前CEO等</label></li></ul>`
+
+//问题3
+var listcon3 = `<h4>3.知道哪些专业是可以走ELS双录取的么？（多选）</h4>
+<ul><li><label for="checkbox31"><input id="checkbox31" type="checkbox" name="checkbox31" value="1">心理学院</label></li>
+<li><label for="checkbox32"><input id="checkbox32" type="checkbox" name="checkbox32" value="2">管理学院</label></li>
+<li><label for="checkbox33"><input id="checkbox33" type="checkbox" name="checkbox33" value="3">国际发展与城市环境学院</label></li>
+<li><label for="checkbox34"><input id="checkbox34" type="checkbox" name="checkbox34" value="4">研究学院</label></li>
+<li><label for="checkbox35"><input id="checkbox35" type="checkbox" name="checkbox35" value="5">工程学院</label></li>
+<li><label for="checkbox36"><input id="checkbox36" type="checkbox" name="checkbox36" value="6">职业教育学院</label></li></ul>`
+
+//问题4
+var listcon4 = `<h4>4.ELS双录取达标要求高不高？马上告诉你（单选）</h4>
+<ul><li><label for="radio41"><input id="radio41" type="radio" name="radio4" value="1">无需语言，直接申请双录取，GPA3.0，无需GMAT/GRE</label></li>
+<li><label for="radio42"><input id="radio42" type="radio" name="radio4" value="2">ELS 112，GPA 3.0， GMAT 620（视专业而定）</label></li>
+<li><label for="radio43"><input id="radio43" type="radio" name="radio4" value="3">可以雅思、托福成绩作为参考，GPA 3.2，GMAT 620（视专业而定）</label></li>
+<li><label for="radio44"><input id="radio44" type="radio" name="radio4" value="4">ELS 110， GPA 3.2， GMAT 620（视专业而定）</label></li></ul>`
+
+//问题5
+var listcon5 = `<h4>5.ELS独创衡量标准-我们也是可以跟IELTS 、TOEFL平起平坐的？（单选）</h4>
+<ul><li><label for="radio51"><input id="radio51" type="radio" name="radio5" value="1">无需语言，直接申请双录取，无需GMAT/GRE</label></li>
+<li><label for="radio52"><input id="radio52" type="radio" name="radio5" value="2">ELS 112，GPA 3.0， GMAT 620</label></li>
+<li><label for="radio53"><input id="radio53" type="radio" name="radio5" value="3">可以雅思、托福成绩作为参考，GPA 3.2，GMAT 620（视专业而定）</label></li>
+<li><label for="radio54"><input id="radio54" type="radio" name="radio5" value="4">ELS 110， GPA 3.2， GMAT 620</label></li></ul>`
+
+//表单
+var forms = `<ul>
+		<li><input type="text" placeholder="姓名"></li>
+		<li><input type="text" placeholder="手机"></li>
+		<li><input type="text" placeholder="机构名称"></li>
+		<li><button type="button">记得留下联系方式~</button></li>
+	</ul>`
